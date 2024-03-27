70479029789e11002a8cf4a18afc3f9f5ddc3ec3
ibrahimOzocak <i.ozocak45@gmail.com>
2024-03-27 16:58:46 +0100

    bug

---
+++ projettodos/srcDuTp/components/TodoItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <li>
+        {{ item.text }}
+        <input type="button"
+        class="btn btn-danger"
+        value="SUPPRIMER" @click="remove">
+        <input type="button"
+        class="btn btn-danger"
+        value="EDIT" @click="edit">
+    </li>
+</template>
+  
+<script>
+export default {
+    props: {
+        item: Object
+    },
+    methods: {
+        remove: function() {
+            this.$emit('remove', this.item.id);
+        },
+        edit: function() {
+            this.$emit('edit', this.item.id);
+        }
+    },
+    emits: ['remove', 'edit']
+}
+</script>
